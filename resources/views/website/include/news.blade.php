8e033adacaaf07c8574b9e2aeeb1139d2b4fcbe6
Muhammad Ilyas <danish.ilyas786@gmail.com>
2025-12-29 14:34:01 +0500

    wip

--- resources/views/website/include/news.blade.php
+++ resources/views/website/include/news.blade.php
@@ -1,66 +1,17 @@
-<style>
-    .notice-row {
-        background-color: #ff0000;
-        border-radius: 5px;
-        margin: 0;
-    }
-
-    .notice-row>div {
-        padding: 0;
-    }
-
-    .notice-ticker {
-        overflow: hidden;
-        white-space: nowrap;
-        padding: 6px 0;
-    }
-
-    .notice-track {
-        display: inline-flex;
-        align-items: center;
-        animation: notice-scroll 25s linear infinite;
-        will-change: transform;
-    }
-
-    .notice-item {
-        font-size: 25px;
-        font-weight: 700;
-        color: #ffffff;
-        margin-right: 40px;
-        white-space: nowrap;
-    }
-
-    /* animation */
-    @keyframes notice-scroll {
-        from {
-            transform: translateX(100%);
-        }
-
-        to {
-            transform: translateX(-100%);
-        }
-    }
-</style>
-
 <div class="container-fluid content">
     @if (isset($activeNews) && $activeNews->isNotEmpty())
-        <section class="content-header">
-            <div class="row notice-row">
-                <div class="col-lg-12 p-0">
-                    <div class="notice-ticker">
-                        <div class="notice-track">
-                            @foreach ($activeNews as $news)
-                                <span class="notice-item">{{ $news->name }}</span>
-                            @endforeach
-
-                            {{-- duplicate for seamless loop --}}
-                            @foreach ($activeNews as $news)
-                                <span class="notice-item">{{ $news->name }}</span>
-                            @endforeach
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+    <section class="content-header">
+      <div class="row" style="background-color: #ff0000; padding: 10px; border-radius: 5px;">
+          <div class="col-lg-12">
+              <marquee class="mt-2 pb-2" style="color:white; line-height: 1.8; padding-top: 8px;" direction="right">
+                @foreach ($activeNews as $news)
+                  <b style="font-size:23px">
+                        {{$news->name}}
+                  </b>
+                  @endforeach
+              </marquee>
+          </div>
+      </div>
+    </section>
     @endif
 </div>
