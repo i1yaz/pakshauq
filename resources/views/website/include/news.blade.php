6b16d905db1f0c79109c4734f8d9554be08f2daa
Muhammad Ilyas <danish.ilyas786@gmail.com>
2025-12-29 14:22:37 +0500

    wip

--- resources/views/website/include/news.blade.php
+++ resources/views/website/include/news.blade.php
@@ -3,7 +3,7 @@
     <section class="content-header">
       <div class="row" style="background-color: #ff0000; padding: 10px; border-radius: 5px;">
           <div class="col-lg-12">
-              <marquee class="mt-2 pb-2" style="color:white; line-height: 1.8; padding-top: 8px;" direction="right">
+              <marquee style="color:white; line-height: 1.8; padding-top: 8px;" direction="right">
                 @foreach ($activeNews as $news)
                   <b style="font-size:25px">
                         {{$news->name}}
